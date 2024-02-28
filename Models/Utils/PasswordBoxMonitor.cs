f930c3e1ec1084a2c275cc56534b6c4baec9ac7f
Abo Bus <egorik2706@gmail.com>
2024-02-28 12:57:45 +0300

    Замена string на SecureString при использовании пароля

---
+++ Models/Utils/PasswordBoxMonitor.cs
@@ -0,0 +1,51 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace HRIS_Software.Models.Utils
+{
+    internal sealed class PasswordBoxMonitor : DependencyObject
+    {
+        public static readonly DependencyProperty IsMonitoringProperty =
+            DependencyProperty.RegisterAttached(
+                "IsMonitoring",
+                typeof(bool),
+                typeof(PasswordBoxMonitor),
+                new UIPropertyMetadata(false, OnIsMonitorChanged));
+
+        public static bool GetIsMonitoring(DependencyObject obj) => (bool)obj.GetValue(IsMonitoringProperty);
+        public static void SetIsMonitoring(DependencyObject obj, bool value) => obj.SetValue(IsMonitoringProperty, value);
+
+        public static readonly DependencyProperty PasswordLengthProperty =
+            DependencyProperty.RegisterAttached(
+                "PasswordLength",
+                typeof(int),
+                typeof(PasswordBoxMonitor),
+                new UIPropertyMetadata(default(int)));
+
+        public static int GetPasswordLength(DependencyObject obj) => (int)obj.GetValue(PasswordLengthProperty);
+        public static void SetPasswordLength(DependencyObject obj, int value) => obj.SetValue(PasswordLengthProperty, value);
+
+        private static void OnIsMonitorChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (d is PasswordBox pb)
+            {
+                if ((bool)e.NewValue)
+                {
+                    pb.PasswordChanged += PasswordChanged;
+                }
+                else
+                {
+                    pb.PasswordChanged -= PasswordChanged;
+                }
+            }
+        }
+
+        private static void PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            if (sender is PasswordBox pb)
+            {
+                SetPasswordLength(pb, pb.SecurePassword.Length);
+            }
+        }
+    }
+}
